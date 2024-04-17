d9e54fe949752810ffda8e2a05c3a83040ad2166
Delfina Piendi <delfinapiendi@gmail.com>
2024-04-16 21:46:20 -0300

    1.0 carrito

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Cart = () => {
     return (
         <div>
-            Carrito
+            <section id="cart-banner" className="d-flex justify-content-center align-items-center">
+                <h1 className="white-1">Carrito</h1>
+            </section>
+            
         </div>
     );
 }

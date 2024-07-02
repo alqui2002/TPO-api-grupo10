b2e8d6eff016b31a8f47b31bd0d4d03a96f7f76f
Delfina Piendi <delfinapiendi@gmail.com>
2024-07-02 17:23:50 -0300

    conexion

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,12 +1,80 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
 import "../assets/css/login.css";
+import UserServiceFetch from "../services/LoginServiceFetch";
 
 const Login = () => {
     const navigate = useNavigate();
     const [hasAccount, setHasAccount] = useState('yes');
     const [forgotP, setForgotP] = useState('no');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const [registerName, setRegisterName] = useState('');
+    const [registerLastName, setRegisterLastName] = useState('');
+    const [registerUsername, setRegisterUsername] = useState('');
+    const [registerPassword, setRegisterPassword] = useState('');
+    const [registerRole, setRegisterRole] = useState('');
+
+    
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:8080/api/v1/auth/authenticate', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                
+                body: JSON.stringify({ username, password }),
+            });
+    
+            if (!response.ok) {
+                throw new Error('Invalid username or password');
+            }
+    
+            const data = await response.json();
+            localStorage.setItem('token', data.token); // Asumiendo que tu backend devuelve un token
+            navigate('/'); // Navega a la página principal después de iniciar sesión
+            console.log("lo lograse!");
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        setRegisterRole('USER');
+        try {
+            const response = await fetch('http://localhost:8080/api/v1/auth/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ 
+                    name: registerName, 
+                    lastname: registerLastName, 
+                    username: registerUsername, 
+                    password: registerPassword,
+                    role: registerRole
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Could not create account');
+            }
+
+            const data = await response.json();
+            localStorage.setItem('token', data.token); // Asumiendo que tu backend devuelve un token
+            navigate('/'); // Navega a la página principal después de registrarse
+            console.log("¡Registro exitoso!");
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
 
     const renderForgotP = () => {
         switch (forgotP) {
@@ -27,28 +95,67 @@ const Login = () => {
                 return (
                     <div className="d-flex flex-column align-items-center">
                         <div className="padding-nav"></div>
-                        <form id="login-form" className="background-color-1 d-flex flex-column align-items-center">
-                            <h3 className="fw-bold">Registrate</h3>
-                            <input type="text" name="title" placeholder="Nombre" />
-                            <input type="text" name="title" placeholder="Apellido" />
-                            <input type="text" name="title" placeholder="Usuario" />
-                            <input type="text" name="subtitle" placeholder="Contraseña" />
-                            <button className="background-color-3 white-1" onClick={() => navigate('/')}>Crear cuenta</button>
+                        <form id="login-form" className="background-color-1 d-flex flex-column align-items-center" onSubmit={handleRegister}>
+                            <h3 className="fw-bold">Regístrate</h3>
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="Nombre"
+                                value={registerName}
+                                onChange={(e) => setRegisterName(e.target.value)}
+                            />
+                            <input
+                                type="text"
+                                name="lastname"
+                                placeholder="Apellido"
+                                value={registerLastName}
+                                onChange={(e) => setRegisterLastName(e.target.value)}
+                            />
+                            <input
+                                type="text"
+                                name="username"
+                                placeholder="Usuario"
+                                value={registerUsername}
+                                onChange={(e) => setRegisterUsername(e.target.value)}
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Contraseña"
+                                value={registerPassword}
+                                onChange={(e) => setRegisterPassword(e.target.value)}
+                            />
+                            {error && <div className="error-message">{error}</div>}
+                            <button className="background-color-3 white-1" type="submit">Crear cuenta</button>
                         </form>
                         <p className="m-2 mt-5">¿Ya tenés cuenta?</p>
                         <button className="button-1 px-3" onClick={() => setHasAccount("yes")}>Iniciar sesión</button>
                     </div>
                 );
             default:
-                return(
+                return (
                     <div className="d-flex flex-column align-items-center">
                         <div className="padding-nav"></div>
-                        <form id="login-form" className="background-color-1 d-flex flex-column align-items-center">
+                        <form id="login-form" className="background-color-1 d-flex flex-column align-items-center" onSubmit={handleLogin}>
                             <h3 className="fw-bold">Iniciar Sesión</h3>
-                            <input type="text" name="title" placeholder="Usuario" />
-                            <input id="login-input-2" type="text" name="subtitle" placeholder="Contraseña" />
+                            <input
+                                type="text"
+                                name="username"
+                                placeholder="Usuario"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                            <input
+                                id="login-input-2"
+                                type="password"
+                                name="password"
+                                placeholder="Contraseña"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                             {renderForgotP()}
-                            <button className="background-color-3 white-1" onClick={() => navigate('/')}>Iniciar Sesion</button>
+                            {error && <div className="error-message">{error}</div>}
+                            <button className="background-color-3 white-1" type="submit">Iniciar Sesión</button>
                         </form>
                         <p className="m-2 mt-5">¿No tenés cuenta?</p>
                         <button className="button-1 px-3" onClick={() => setHasAccount("no")}>Registrarse</button>
